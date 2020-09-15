7096ddbe2b2011b8696a51cd61701df1b5de974b
cesar caceres <cesarc770@gmail.com>
2020-09-15 14:30:55 -0600

    initial commit

---
+++ js/sceneMain.js
@@ -0,0 +1,15 @@
+class SceneMain extends Phaser.Scene {
+    constructor() {
+        super('SceneMain');
+    }
+    preload()
+    {
+    	//Load Images or Sounds here
+    }
+    create() {
+        //Define objects
+    }
+    update() {
+        //Constant running loop
+    }
+}
