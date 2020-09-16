bf306d2fd5ef6b924b70cee84d14529772ea0b73
cesar caceres <cesarc770@gmail.com>
2020-09-15 20:10:59 -0600

    added logic to track mouse movement over board

--- js/classes/board.js
+++ js/classes/board.js
@@ -46,38 +46,70 @@ class Board extends Phaser.GameObjects.Container
               [256, 255],
             ]
           ];
-          
+
         this.scene.add.existing(this);
     }
 
-    defineTubes()
+    makeLanes()
     {
-        this.tubeQuads = [];
+        this.Lanes = [];
         for (let i = 0; i < this.shape[0].length - 1; i++) {
-          this.tubeQuads.push([
+          this.Lanes.push([
             this.shape[0][i],
             this.shape[0][i + 1],
             this.shape[1][i + 1],
             this.shape[1][i],
           ]);
         }
-        //this.tubeCenter = Util.tubeCenter(tubeShape);
     }
 
-    drawTubeQuads() {
+    drawLanes() {
         this.graphics.lineStyle(4, 0xff0000);
     
-        for (let i = 0; i < this.tubeQuads.length; i++) {
-          const tubeQuad = this.tubeQuads[i];
-          console.log(tubeQuad)
+        for (let i = 0; i < this.Lanes.length; i++) {
+          const Lane = this.Lanes[i];
           this.graphics.beginPath();
           
-          this.graphics.moveTo(...tubeQuad[0]);
-          this.graphics.lineTo(...tubeQuad[1]);
-          this.graphics.lineTo(...tubeQuad[2]);
-          this.graphics.lineTo(...tubeQuad[3]);
+          this.graphics.moveTo(...Lane[0]);
+          this.graphics.lineTo(...Lane[1]);
+          this.graphics.lineTo(...Lane[2]);
+          this.graphics.lineTo(...Lane[3]);
           this.graphics.closePath();
           this.graphics.strokePath();
         }
+      }
+
+      drawOccupiedLane(occupiedLane , color ) {
+        
+        //TODO: find better logic for this method
+
+          for (let i = 0; i < this.Lanes.length; i++) {
+            const lane = this.Lanes[i];
+
+            if (occupiedLane != lane)
+            {
+                this.graphics.lineStyle(4, 0xff0000);
+                this.graphics.beginPath();
+                
+                this.graphics.moveTo(...lane[0]);
+                this.graphics.lineTo(...lane[1]);
+                this.graphics.lineTo(...lane[2]);
+                this.graphics.lineTo(...lane[3]);
+                this.graphics.closePath();
+                this.graphics.strokePath();
+            }
+          }
+
+          this.graphics.lineStyle(4, color);
+
+        this.graphics.beginPath();
+          
+          this.graphics.moveTo(...occupiedLane[0]);
+          this.graphics.lineTo(...occupiedLane[1]);
+          this.graphics.lineTo(...occupiedLane[2]);
+          this.graphics.lineTo(...occupiedLane[3]);
+          this.graphics.closePath();
+          this.graphics.strokePath();
+
       }
 }
--- js/sceneMain.js
+++ js/sceneMain.js
@@ -8,11 +8,44 @@ class SceneMain extends Phaser.Scene {
     }
     create() {
         //Define objects
-        var board = new Board({scene:this}, this.add.graphics());
-        board.defineTubes();
-        board.drawTubeQuads();
+        this.board = new Board({scene:this}, this.add.graphics());
+        this.board.makeLanes();
+        this.board.drawLanes();
+        //add physics
+        
+        this.input.mouse.capture = true;
+
     }
     update() {
         //Constant running loop
+        //console.log(this.input.activePointer.position.x);
+        this.movePointer();
     }
+
+    movePointer()
+    {
+        var point = [this.input.activePointer.position.x, this.input.activePointer.position.y];
+        for (let i = 0; i < this.board.Lanes.length; i++) 
+        {
+        
+            this.boundary = this.board.Lanes[i];
+            if (this.isInside(point, this.boundary)) {
+              this.board.drawOccupiedLane(this.boundary, 0xfff000);
+            }
+        }
+    }
+
+    isInside(point, boundary) {
+        let result = false;
+        
+
+        for (let i = 0, j = boundary.length - 1; i < boundary.length; j = i++) 
+        {
+          if ((boundary[i][1] > point[1]) != (boundary[j][1] > point[1]) && (point[0] < (boundary[j][0] - boundary[i][0]) * (point[1] - boundary[i][1]) / (boundary[j][1] - boundary[i][1]) + boundary[i][0])) 
+          {
+            result = !result;
+          }
+        }
+        return result;
+      }
 }
