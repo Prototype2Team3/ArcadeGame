e7f8568ffdbe09a794d16cb6839f012e9115118b
cesar caceres <cesarc770@gmail.com>
2020-09-20 18:28:04 -0600

    added progress bar for money bar

--- js/components/scorebox.js
+++ js/components/scorebox.js
@@ -5,7 +5,11 @@ class ScoreBox extends Phaser.GameObjects.Container
         super(config.scene);
         this.scene=config.scene;
 
-        this.text1 = this.scene.add.text(0,0, "Money Bar:0");
+        this.progressBar = this.scene.add.graphics();
+        this.progressBox = this.scene.add.graphics();
+        this.progressBox.fillStyle(0x222222, 0.5);
+        this.progressBox.fillRect(50, 50, 105, 25);
+        this.text1 = this.scene.add.text(0,50, "Money Bar");
         this.text1.setOrigin(0.5,0.5);
         this.add(this.text1);
 
@@ -16,6 +20,16 @@ class ScoreBox extends Phaser.GameObjects.Container
     
     scoreUpdated()
     {
-        this.text1.setText("Money bar:" + model.score);
+        //this.text1.setText("Money bar:" + model.score);
+        if(model.score > 0)
+        {
+            this.progressBar.clear();
+            this.progressBar.fillStyle(0xff0000, 1);
+            this.progressBar.fillRect(50, 50, model.score, 25);
+        }
+        else
+        {
+            this.progressBar.clear();
+        }
     }
 }
