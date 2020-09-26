eb3b5b7d62d774016fb446a0db5940077b8a32de
cesar caceres <cesarc770@gmail.com>
2020-09-26 10:50:01 -0600

    added flowr, reduced anger bar and compelted whole new game loop

--- js/components/angerbar.js
+++ js/components/angerbar.js
@@ -6,79 +6,36 @@ class AngerBar extends Phaser.GameObjects.Container
         this.scene=config.scene;
 
         this.angerBar = this.scene.add.image(400, 750, 'anger_container');
-        this.angerIcon = this.scene.add.image(335, 745, 'anger_icon');
-        this.angerBlocks =[];
-        this.angerBlock1 = this.scene.add.image(365, 750, 'anger_block');
-        this.angerBlock2 = this.scene.add.image(383 , 750, 'anger_block');
-        this.angerBlock3 = this.scene.add.image(401, 750, 'anger_block');
-        this.angerBlock4 = this.scene.add.image(419, 750, 'anger_block');
-        this.angerBlock5 = this.scene.add.image(437, 750, 'anger_block');
-        this.angerBlocks.push(this.angerBlock1);
-        this.angerBlocks.push(this.angerBlock2);
-        this.angerBlocks.push(this.angerBlock3);
-        this.angerBlocks.push(this.angerBlock4);
-        this.angerBlocks.push(this.angerBlock5);
+        this.angerIcon = this.scene.add.image(220, 745, 'anger_icon');
+        this.startX = 250;
+        this.interval = 3;
 
-        this.angerBlocks.forEach((block) =>{
-            block.setScale(0.75);
-            block.visible = false;
-        });
+        this.angerBarBlocks = this.scene.add.group();
+
+        for (var i = 0; i < 100; i++)
+        {
+            var item = this.angerBarBlocks.create(this.startX + (this.interval * i), 750, 'anger_block', i);
+            item.setScale(0.2, 1.5);  
+        }
+        this.angerUpdated();
 
         this.scene.add.existing(this);
 
-        emitter.on(G.TIME_UPDATED, this.timeUpdated, this);
+       // emitter.on(G.TIME_UPDATED, this.timeUpdated, this);
     }
     
-    timeUpdated()
+    angerUpdated()
     {
-        //this.text1.setText("Money bar:" + model.score);
-        if(model.timeElapsed >= 0 && model.timeElapsed < 5)
+        for(var i =0; i < 100; i++)
         {
-            this.angerBlocks[0].visible = false;
-            this.angerBlocks[1].visible = false;
-            this.angerBlocks[2].visible = false;
-            this.angerBlocks[3].visible = false;
-            this.angerBlocks[4].visible = false;
-        }
-       else if(model.timeElapsed >= 5 && model.timeElapsed < 10 )
-        {
-            this.angerBlocks[0].visible = true;
-            this.angerBlocks[1].visible = false;
-            this.angerBlocks[2].visible = false;
-            this.angerBlocks[3].visible = false;
-            this.angerBlocks[4].visible = false;
-        }
-        else if(model.timeElapsed >= 10 && model.timeElapsed < 15)
-        {
-            this.angerBlocks[0].visible = true;
-            this.angerBlocks[1].visible = true;
-            this.angerBlocks[2].visible = false;
-            this.angerBlocks[3].visible = false;
-            this.angerBlocks[4].visible = false;
-        }
-        else if(model.timeElapsed >= 15 && model.timeElapsed < 20)
-        {
-            this.angerBlocks[0].visible = true;
-            this.angerBlocks[1].visible = true;
-            this.angerBlocks[2].visible = true;
-            this.angerBlocks[3].visible = false;
-            this.angerBlocks[4].visible = false;
-        }
-        else if(model.timeElapsed >= 20 && model.timeElapsed < 25)
-        {
-            this.angerBlocks[0].visible = true;
-            this.angerBlocks[1].visible = true;
-            this.angerBlocks[2].visible = true;
-            this.angerBlocks[3].visible = true;
-            this.angerBlocks[4].visible = false;
-        }
-       else if(model.timeElapsed >= 25 )
-        {
-            this.angerBlocks[0].visible = true;
-            this.angerBlocks[1].visible = true;
-            this.angerBlocks[2].visible = true;
-            this.angerBlocks[3].visible = true;
-            this.angerBlocks[4].visible = true;
+            if(i < model.anger)
+            {
+                this.angerBarBlocks.children.entries[i].visible = true;
+            }
+            else
+            {
+                this.angerBarBlocks.children.entries[i].visible = false;
+            }
         }
     }
 }
--- js/components/scorebox.js
+++ js/components/scorebox.js
@@ -6,46 +6,19 @@ class ScoreBox extends Phaser.GameObjects.Container
         this.scene=config.scene;
 
         this.counter = 0;
-        this.moneyBar = this.scene.add.image(120, 50, 'money_container');
-        this.moneyBar.setScale(1.5, 1);
 
-        this.moneySign1 = this.scene.add.image(90, 85, 'money_sign');
-        this.moneySign2 = this.scene.add.image(140, 85, 'money_sign');
-        this.moneySign3 = this.scene.add.image(190, 85, 'money_sign');
-        this.updateDollars(model.moneySigns);
+        this.moneyBar = this.scene.add.image(220, 50, 'money_container');
+        this.startX = 70;
+        this.interval = 3;
 
-        this.moneyBlocks =[];
-        this.moneyBlock1 = this.scene.add.image(60, 50, 'money_block');
-        this.moneyBlock2 = this.scene.add.image(72, 50, 'money_block');
-        this.moneyBlock3 = this.scene.add.image(84, 50, 'money_block');
-        this.moneyBlock4 = this.scene.add.image(96, 50, 'money_block');
-        this.moneyBlock5 = this.scene.add.image(108, 50, 'money_block');
-        this.moneyBlock6 = this.scene.add.image(120, 50, 'money_block');
-        this.moneyBlock7 = this.scene.add.image(132, 50, 'money_block');
-        this.moneyBlock8 = this.scene.add.image(144, 50, 'money_block');
-        this.moneyBlock9 = this.scene.add.image(156, 50, 'money_block');
-        this.moneyBlock10 = this.scene.add.image(168, 50, 'money_block');
-        this.moneyBlock11= this.scene.add.image(180, 50, 'money_block');
-        this.moneyBlock12 = this.scene.add.image(192, 50, 'money_block');
+        this.moneyBlocks = this.scene.add.group();
 
-        this.moneyBlocks.push(this.moneyBlock1);
-        this.moneyBlocks.push(this.moneyBlock2);
-        this.moneyBlocks.push(this.moneyBlock3);
-        this.moneyBlocks.push(this.moneyBlock4);
-        this.moneyBlocks.push(this.moneyBlock5);
-        this.moneyBlocks.push(this.moneyBlock6);
-        this.moneyBlocks.push(this.moneyBlock7);
-        this.moneyBlocks.push(this.moneyBlock8);
-        this.moneyBlocks.push(this.moneyBlock9);
-        this.moneyBlocks.push(this.moneyBlock10);
-        this.moneyBlocks.push(this.moneyBlock11);
-        this.moneyBlocks.push(this.moneyBlock12);
-
-        this.moneyBlocks.forEach((block) =>{
-            block.setScale(0.65, 0.75);
-            block.visible = false;
-        });
-
+        for (var i = 0; i < 100; i++)
+        {
+            var item = this.moneyBlocks.create(this.startX + (this.interval * i), 50, 'money_block', i);
+            item.setScale(0.2, 1.5);  
+        }
+        this.scoreUpdated();
         this.scene.add.existing(this);
 
         emitter.on(G.SCORE_UPDATED, this.scoreUpdated, this);
@@ -53,287 +26,18 @@ class ScoreBox extends Phaser.GameObjects.Container
     
     scoreUpdated()
     {
-        if(model.score >= 5 && model.score < 10)
+
+        for(var i =0; i < 100; i++)
         {
-            for (var i = 0; i < this.moneyBlocks.length; i++)
+            if(i < model.score)
             {
-                if ( i != 0)
-                {
-                    this.moneyBlocks[i].visible = false;
-                }
-                else
-                {
-                    this.moneyBlocks[i].visible = true;
-                }
+                this.moneyBlocks.children.entries[i].visible = true;
+            }
+            else
+            {
+                this.moneyBlocks.children.entries[i].visible = false;
             }
         }
-       else if(model.score >= 10 && model.score < 15 )
-        {
-            for (var i = 0; i < this.moneyBlocks.length; i++)
-            {
-                switch(i)
-                {
-                    case 0:
-                    case 1:
-                        this.moneyBlocks[i].visible = true;
-                        break;
-                    default:
-                        this.moneyBlocks[i].visible = false;
-                        break;
-                }
-            }
-        }
-        else if(model.score >= 15 && model.score < 20)
-        {
-            for (var i = 0; i < this.moneyBlocks.length; i++)
-            {
-                switch(i)
-                {
-                    case 0:
-                    case 1:
-                    case 2:
-                        this.moneyBlocks[i].visible = true;
-                        break;
-                    default:
-                        this.moneyBlocks[i].visible = false;
-                        break;
-                }
-            }
-  
-        }
-        else if(model.score >=20 && model.score < 25)
-        {
-            for (var i = 0; i < this.moneyBlocks.length; i++)
-            {
-                switch(i)
-                {
-                    case 0:
-                    case 1:
-                    case 2:
-                    case 3:
-                        this.moneyBlocks[i].visible = true;
-                        break;
-                    default:
-                        this.moneyBlocks[i].visible = false;
-                        break;
-                }
-            }
-        }
-        else if(model.score >= 25 && model.score < 30)
-        {
-            for (var i = 0; i < this.moneyBlocks.length; i++)
-            {
-                switch(i)
-                {
-                    case 0:
-                    case 1:
-                    case 2:
-                    case 3:
-                    case 4:
-                        this.moneyBlocks[i].visible = true;
-                        break;
-                    default:
-                        this.moneyBlocks[i].visible = false;
-                        break;
-                }
-            }
-            
-        }
-        else if(model.score >= 30 && model.score < 35)
-        {
-            for (var i = 0; i < this.moneyBlocks.length; i++)
-            {
-                switch(i)
-                {
-                    case 0:
-                    case 1:
-                    case 2:
-                    case 3:
-                    case 4:
-                    case 5:
-                        this.moneyBlocks[i].visible = true;
-                        break;
-                    default:
-                        this.moneyBlocks[i].visible = false;
-                        break;
-                }
-            }
-            
-        }
-        else if(model.score >= 35 && model.score < 40)
-        {
-            for (var i = 0; i < this.moneyBlocks.length; i++)
-            {
-                switch(i)
-                {
-                    case 0:
-                    case 1:
-                    case 2:
-                    case 3:
-                    case 4:
-                    case 5:
-                    case 6:
-                        this.moneyBlocks[i].visible = true;
-                        break;
-                    default:
-                        this.moneyBlocks[i].visible = false;
-                        break;
-                }
-            }       
-        }
-        else if(model.score >= 40 && model.score < 45)
-        {
-            for (var i = 0; i < this.moneyBlocks.length; i++)
-            {
-                switch(i)
-                {
-                    case 0:
-                    case 1:
-                    case 2:
-                    case 3:
-                    case 4:
-                    case 5:
-                    case 6:
-                    case 7:
-                        this.moneyBlocks[i].visible = true;
-                        break;
-                    default:
-                        this.moneyBlocks[i].visible = false;
-                        break;
-                }
-            }       
-        }
-        else if(model.score >= 45 && model.score < 50)
-        {
-            for (var i = 0; i < this.moneyBlocks.length; i++)
-            {
-                switch(i)
-                {
-                    case 0:
-                    case 1:
-                    case 2:
-                    case 3:
-                    case 4:
-                    case 5:
-                    case 6:
-                    case 7:
-                    case 8:
-                        this.moneyBlocks[i].visible = true;
-                        break;
-                    default:
-                        this.moneyBlocks[i].visible = false;
-                        break;
-                }
-            }       
-        }
-        else if(model.score >= 50 && model.score < 55)
-        {
-            for (var i = 0; i < this.moneyBlocks.length; i++)
-            {
-                switch(i)
-                {
-                    case 0:
-                    case 1:
-                    case 2:
-                    case 3:
-                    case 4:
-                    case 5:
-                    case 6:
-                    case 7:
-                    case 8:
-                    case 9:
-                        this.moneyBlocks[i].visible = true;
-                        break;
-                    default:
-                        this.moneyBlocks[i].visible = false;
-                        break;
-                }
-            }       
-        }
-        else if(model.score >= 55 && model.score < 60)
-        {
-            for (var i = 0; i < this.moneyBlocks.length; i++)
-            {
-                switch(i)
-                {
-                    case 0:
-                    case 1:
-                    case 2:
-                    case 3:
-                    case 4:
-                    case 5:
-                    case 6:
-                    case 7:
-                    case 8:
-                    case 9:
-                    case 10:
-                        this.moneyBlocks[i].visible = true;
-                        break;
-                    default:
-                        this.moneyBlocks[i].visible = false;
-                        break;
-                }
-            }       
-        }
-        else if(model.score >= 60 || model.score >= 0)
-        {
-            for (var i = 0; i < this.moneyBlocks.length; i++)
-            {
-                switch(i)
-                {
-                    case 0:
-                    case 1:
-                    case 2:
-                    case 3:
-                    case 4:
-                    case 5:
-                    case 6:
-                    case 7:
-                    case 8:
-                    case 9:
-                    case 10:
-                    case 11:
-                        this.moneyBlocks[i].visible = false;
-                        break;
-                    default:
-                        this.moneyBlocks[i].visible = false;
-                        break;
-                }
-            }       
-        }
+
     }
-
-    updateDollars(money)
-    {
-            switch (money)
-            {
-            case 0:
-                this.moneySign1.visible = false;
-                this.moneySign2.visible = false;
-                this.moneySign3.visible = false;
-                break;
-            case 1:
-                this.moneySign1.visible = true;
-                this.moneySign2.visible = false;
-                this.moneySign3.visible = false;
-                break;
-            case 2:
-                this.moneySign1.visible = true;
-                this.moneySign2.visible = true;
-                this.moneySign3.visible = false;
-                break;
-            case 3:
-                this.moneySign1.visible = true;
-                this.moneySign2.visible = true;
-                this.moneySign3.visible = true;
-                break;
-            default:
-                this.moneySign1.visible = false;
-                this.moneySign2.visible = false;
-                this.moneySign3.visible = false;
-                break;
-
-
-            }
-        }
 }
