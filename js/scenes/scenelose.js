324b82def6af8349ffe0326bb4007962779f8f25
cesar caceres <cesarc770@gmail.com>
2020-09-28 21:38:20 -0600

    added sounds

--- js/scenes/scenelose.js
+++ js/scenes/scenelose.js
@@ -6,6 +6,8 @@ class SceneLose extends Phaser.Scene {
     {
         this.load.image("lose_back", "images/VectorArt/Losing_Screen.png");
         this.load.image("play_btn", "images/VectorArt/button.png");
+        //load sounds
+        this.load.audio('lose', 'sounds/LoseScreen.wav');
     }
     create() {
         console.log("SceneLoose!");
@@ -18,6 +20,9 @@ class SceneLose extends Phaser.Scene {
         this.startButton = new Button({scene:this, key: 'play_btn', text:'Play AGAIN', x: 400, y: 750, event: 'play_pressed', params: 'play_again'});
 
         emitter.on('play_pressed', this.playPressed, this);
+
+        this.loseSound=this.sound.add('lose');
+        this.played = false;
         
     }
 
@@ -28,5 +33,13 @@ class SceneLose extends Phaser.Scene {
     }
 
 
-    update() {}
+    update() {
+
+        if(!this.played)
+        {
+            console.log("HERE")
+            this.loseSound.play();
+            this.played = true;
+        }
+    }
 }
